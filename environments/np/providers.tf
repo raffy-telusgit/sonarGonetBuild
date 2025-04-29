2d8871c0a7f223ff213a4255b712a9a6e732cae9
raffy-telusgit <raphael.cordero@telus.com>
2025-04-29 12:01:29 -0700

    modified backend.tf

--- environments/np/providers.tf
+++ environments/np/providers.tf
@@ -5,13 +5,10 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "gcs" {
-    bucket = "sonar-gonet-lab-env-lab-tfstate"
-  }
 }
 
 provider "google" {
-  project = "sonar-gonet-lab-env-lab-0308cf"
+  project = var.project
   region  = var.region
   zone    = var.zone
 }
