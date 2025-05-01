995b4c7003c5e521f01b9a4f4d852a47e7851212
raffy-telusgit <raphael.cordero@telus.com>
2025-04-30 21:32:49 -0700

    feat: add output for VPC network self-link in VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -53,3 +53,8 @@ output "subnet_name" {
   description = "The name of the subnet"
   value       = google_compute_subnetwork.subnet.name
 }
+
+output "network_self_link" {
+  description = "The self-link of the VPC network"
+  value       = google_compute_network.vpc_network.self_link
+}
